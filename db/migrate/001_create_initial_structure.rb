5f4a2b68faa3d6e42042f91654a964e2a06f9eb3
Joshua Harlan Lifton <lifton@puppetlabs.com>
2012-05-10 17:07:25 -0700

    Revert change to migration script

--- db/migrate/001_create_initial_structure.rb
+++ db/migrate/001_create_initial_structure.rb
@@ -26,7 +26,6 @@ class CreateInitialStructure < ActiveRecord::Migration
       t.timestamp 'created_on',       :null => false
       t.string    'client_hostname',  :null => false
       t.string    'username',         :null => false
-      t.string    'authenticator',    :null => false
       t.text      'extra_attributes', :null => true
     end
 
