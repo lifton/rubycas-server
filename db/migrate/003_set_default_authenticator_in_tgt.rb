0d0ad44593832968385570c9b84cf1b35542b401
Joshua Harlan Lifton <lifton@puppetlabs.com>
2012-09-20 14:59:06 -0700

    Add migration for previously added authenticator column in casserver_tgt
    Previous to this commit, an up-to-date RubyCAS-Server would crash upon
    encountering a session that was created before the "authenticator"
    column had been added to the casserver_tgt table. This commit is not a
    full fix for this problem, but it does prevent the problem from
    occurring if the session is retrieved from the server rather than the
    cookie.

---
+++ db/migrate/003_set_default_authenticator_in_tgt.rb
@@ -0,0 +1,10 @@
+class SetDefaultAuthenticatorInTgt < ActiveRecord::Migration
+  def self.up
+    change_column 'casserver_tgt', 'authenticator', :string, :limit => 64, :null => false, :default => 'CASServer::Authenticators::SQLEncrypted'
+    execute "UPDATE `casserver_tgt` SET `authenticator` =  'CASServer::Authenticators::SQLEncrypted' WHERE `authenticator` = NULL;"
+  end
+
+  def self.down
+    change_column 'casserver_tgt', 'authenticator', :string, :limit => 64, :null => false, :default => nil
+  end
+end
